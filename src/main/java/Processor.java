84edae9bd94dfbc8ed986f8b47c3b1d49b98fcdb
gerald <gerald.even@gmail.com>
2018-07-03 12:14:44 +0200

    API test

--- src/main/java/Processor.java
+++ src/main/java/Processor.java
@@ -5,13 +5,17 @@ import org.apache.commons.jexl3.MapContext;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class Processor {
-    public static AggregatorContext process(Object o)throws Exception{
+    public static AggregatorContext process(Object o) throws Exception {
         return process(o, new AggregatorContext());
     }
-    public static AggregatorContext process(Object o,AggregatorContext aggregatorContext) throws Exception {
+
+    public static AggregatorContext process(Object o, AggregatorContext aggregatorContext) throws Exception {
         Map<String, String> executors = new HashMap<>();
         process("o", o, aggregatorContext, executors, aggregatorContext);
         aggregatorContext.set("o", o);
@@ -30,71 +34,69 @@ public class Processor {
             return;
         if (o.getClass().getPackage() != null && "java.lang".equals(o.getClass().getPackage().getName()))
             return;
-        for (Field f : o.getClass().getDeclaredFields()) {
-            Execute executors[] = f.getDeclaredAnnotationsByType(Execute.class);
-            Collect collects[] = f.getDeclaredAnnotationsByType(Collect.class);
-            if (executors != null && executors.length > 0) {
-                for (Execute execute : executors) {
-                    if (applicable(o, execute.when())) {
-                        executorsMap.put(prefix + "." + f.getName(), execute.value());
-                    }
-                }
-            } else if (collects != null && collects.length > 0) {
-                if (f.get(o) != null) {
-                    String add = prefix + "." + f.getName();
-                    for (Collect collect : collects) {
-                        if (applicable(o, collect.when())) {
-                            aggregatorContext.addFormula(collect.value(), add);
+        //Check if the object is a collection
+        Class objectClass = o.getClass();
+        if (objectClass.isArray()) {
+            int length = Array.getLength(o);
+            for (int i = 0; i < length; i++) {
+                process(prefix + "[" + i + "]", Array.get(o, i), aggregatorContext, executorsMap,
+                        localContext);
+            }
+        } else if (List.class.isAssignableFrom(objectClass)) {
+            @SuppressWarnings("rawtypes")
+            List l = (List) o;
+            for (int i = 0; i < l.size(); i++) {
+                process(prefix + "[" + i + "]", l.get(i), aggregatorContext, executorsMap, localContext);
+            }
+            //Not working ... why?
+            //} else if (Map.class.isAssignableFrom(fieldClass)) {
+        } else if (o instanceof Map) {
+            @SuppressWarnings("rawtypes")
+            Map m = (Map) o;
+            String key = prefix.replaceAll("\\.", "_") + "_";
+            int i = 0;
+            for (Object mO : m.values()) {
+                String setKey = key + (i++);
+                localContext.set(setKey, mO);
+                process(setKey, mO, aggregatorContext, executorsMap, localContext);
+            }
+        } else if (Iterable.class.isAssignableFrom(objectClass)) {
+            @SuppressWarnings("rawtypes")
+            Iterator it = ((Iterable) o).iterator();
+            int i = 0;
+            String key = prefix.replaceAll("\\.", "_") + "_";
+            while (it.hasNext()) {
+                String setKey = key + (i++);
+                Object iO = it.next();
+                localContext.set(setKey, iO);
+                process(setKey, iO, aggregatorContext, executorsMap, localContext);
+            }
+        } else {
+            //Check the fields they can be Collectors, Executors or simple fields to process
+            for (Field f : o.getClass().getDeclaredFields()) {
+                Execute executors[] = f.getDeclaredAnnotationsByType(Execute.class);
+                Collect collects[] = f.getDeclaredAnnotationsByType(Collect.class);
+                if (executors != null && executors.length > 0) {
+                    for (Execute execute : executors) {
+                        if (applicable(o, execute.when())) {
+                            executorsMap.put(prefix + "." + f.getName(), execute.value());
                         }
                     }
-                }
-            } else {
-                Object fieldValue = f.get(o);
-                if (fieldValue != null) {
-                    Class fieldClass = f.getType();
-                    if (fieldClass.isArray()) {
-                        int length = Array.getLength(fieldValue);
-                        for (int i = 0; i < length; i++) {
-                            process(prefix + "." + f.getName() + "[" + i + "]", Array.get(fieldValue, i), aggregatorContext, executorsMap,
-                                    localContext);
+                } else if (collects != null && collects.length > 0) {
+                    if (f.get(o) != null) {
+                        String add = prefix + "." + f.getName();
+                        for (Collect collect : collects) {
+                            if (applicable(o, collect.when())) {
+                                aggregatorContext.addFormula(collect.value(), add);
+                            }
                         }
-                    } else if (List.class.isAssignableFrom(fieldClass)) {
-                        @SuppressWarnings("rawtypes")
-                        List l = (List) fieldValue;
-                        for (int i = 0; i < l.size(); i++) {
-                            process(prefix + "." + f.getName() + "[" + i + "]", l.get(i), aggregatorContext, executorsMap, localContext);
-                        }
-                        //Not working ... why?
-                        //} else if (Map.class.isAssignableFrom(fieldClass)) {
-                    } else if (fieldValue instanceof Map) {
-                        @SuppressWarnings("rawtypes")
-                        Map m = (Map) fieldValue;
-                        String key = prefix.replaceAll("\\.", "_") + "_" + f.getName() + "_";
-                        int i = 0;
-                        for (Object mO : m.values()) {
-                            String setKey = key + (i++);
-                            localContext.set(setKey, mO);
-                            process(setKey, mO, aggregatorContext, executorsMap, localContext);
-                        }
-                    } else if (Iterable.class.isAssignableFrom(fieldClass)) {
-                        @SuppressWarnings("rawtypes")
-                        Iterator it = ((Iterable) fieldValue).iterator();
-                        int i = 0;
-                        String key = prefix.replaceAll("\\.", "_") + "_" + f.getName() + "_";
-                        while (it.hasNext()) {
-                            String setKey = key + (i++);
-                            Object iO = it.next();
-                            localContext.set(setKey, iO);
-                            process(setKey, iO, aggregatorContext, executorsMap, localContext);
-                        }
-                    } else {
-                        process(prefix + "." + f.getName(), f.get(o), aggregatorContext, executorsMap, localContext);
                     }
+                } else {
+                    process(prefix + "." + f.getName(), f.get(o), aggregatorContext, executorsMap, localContext);
                 }
             }
         }
     }
-
 
     private static boolean applicable(Object o, String when) {
         if (when == null || "".equals(when)) return true;
--- src/test/java/ProcessorTest.java
+++ src/test/java/ProcessorTest.java
@@ -1,46 +1,61 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 public class ProcessorTest {
     Business b;
+    AggregatorContext myAggregatorContext;
+
     @Before
-    public void initBusiness(){
-        b=new Business();
-        b.myGrandTotals=new HashMap<>();
-        b.myGrandTotals.put("a",new GrandTotal(0,"a"));
-        b.myGrandTotals.put("b",new GrandTotal(0,"b"));
-        b.myGrandTotals.put("c",new GrandTotal(0,"c"));
-        b.elements=new Row[]{new Row(10,"a"),null,new Row(20,"b")};
-        b.elements2=new Row2[]{new Row2(8),new Row2(null), new Row2(5)};
+    public void initBusiness() {
+        b = new Business();
+        b.myGrandTotals = new HashMap<>();
+        b.myGrandTotals.put("a", new GrandTotal(0, "a"));
+        b.myGrandTotals.put("b", new GrandTotal(0, "b"));
+        b.myGrandTotals.put("c", new GrandTotal(0, "c"));
+        b.elements = new Row[]{new Row(10, "a"), null, new Row(20, "b")};
+        b.elements2 = new Row2[]{new Row2(8), new Row2(null), new Row2(5)};
         b.elements3 = new ArrayList<Row2>();
         b.elements3.add(new Row2(5));
         b.elements4 = new HashSet<Rows>();
-        b.elements4.add(new Row(7,"c"));
+        b.elements4.add(new Row(7, "c"));
         b.elements4.add(new Row2(2));
         b.elements5 = new Hashtable<>();
-        b.elements5.put("a",new Row(1,"a"));
-        b.elements5.put("b",new Row2(3));
-        b.elements5.put("c",new Row2(3));
+        b.elements5.put("a", new Row(1, "a"));
+        b.elements5.put("b", new Row2(3));
+        b.elements5.put("c", new Row2(3));
     }
+
+    @Before
+    public void initContext() {
+        myAggregatorContext = new AggregatorContext();
+        //Adding custom functions to the context
+        myAggregatorContext.register("my", Functions.class);
+    }
+
     @Test
     public void test() throws Exception {
-        AggregatorContext myAggregatorContext = new AggregatorContext();
-        myAggregatorContext.register("my", Functions.class);
-        Processor.process(b,myAggregatorContext);
+        Processor.process(b, myAggregatorContext);
         Assert.assertEquals(new Integer(38), b.total);
         Assert.assertEquals(new Integer(26), b.total2);
         Assert.assertEquals(new Integer(11), b.myGrandTotals.get("a").sum);
         Assert.assertEquals(new Integer(20), b.myGrandTotals.get("b").sum);
         Assert.assertEquals(new Integer(33), b.myGrandTotals.get("c").sum);
         Assert.assertEquals(new Integer(12), b.doubleCount);
-        Assert.assertEquals(4.3333,b.avg2,0.0001);
-        Assert.assertEquals(22.822,b.rate,0.0001);
+        Assert.assertEquals(4.3333, b.avg2, 0.0001);
+        Assert.assertEquals(22.822, b.rate, 0.0001);
         Assert.assertEquals("[a,b,c,a]", b.ccm2);
+    }
+    @Test
+    public void testApi()throws Exception{
+        Processor.process(b.elements5, myAggregatorContext);
+        Object result = myAggregatorContext.evaluate("sum('total')+sum('total2')");
+        Assert.assertEquals(Integer.class,result.getClass());
+        Assert.assertEquals(new Integer(7),(Integer)result);
     }
 }
