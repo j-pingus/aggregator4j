f04733c680dca25b86e0e8c3456c9289112a9ed4
gerald <gerald.even@gmail.com>
2018-09-12 21:21:48 +0200

    first parseable example

--- src/test/java/com/github/jpingus/ParseConfig.java
+++ src/test/java/com/github/jpingus/ParseConfig.java
@@ -1,4 +1,26 @@
 package com.github.jpingus;
 
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class ParseConfig {
+    @Test
+    public void parse() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document docConfig =
+                builder.parse(ParseConfig.class.getResourceAsStream("/config.xml"));
+        analyse(docConfig);
+    }
+
+    private void analyse(Document docConfig) {
+        if(!"aggregator4j".equals(docConfig.getDocumentElement().getTagName()))
+            throw new Error("config root must be aggregator4j");
+    }
 }
